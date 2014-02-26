57bdb10ac10cff36c46083103ad7b9fa280c89d9
Simon Kornblith <simon@simonster.com>
2014-02-26 16:57:51 -0500

    Initial code commit

--- README.md
+++ README.md
@@ -1,3 +1,89 @@
 # VML
 
 [![Build Status](https://travis-ci.org/simonster/VML.jl.png)](https://travis-ci.org/simonster/VML.jl)
+
+This package provides bindings to the Intel Vector Math Library for
+arithmetic and transcendental functions. It is often substantially
+faster than using Julia's built-in operations.
+
+## Using VML.jl
+
+Currently, VML.jl is configured to use `libmkl_vml_avx`, which requires
+AVX support. If your system does not have AVX (e.g., most pre-Sandy
+Bridge systems), you will need to modify the `const lib` declaration at
+the top of `src/VML.jl`. Future versions of VML.jl may automatically
+detect CPU architecture.
+
+After loading VML.jl, vector calls to functions listed below will
+automatically use VML instead of openlibm when possible.
+
+By default, VML uses `VML_HA` mode, which corresponds to an accuracy of
+<1 ulp. To specify low accuracy, use `vml_set_mode(VML_LA)`. To specify
+enhanced performance, use `vml_set_mode(VML_EP)`. More documentation
+regarding these options is available on [Intel's website](http://software.intel.com/sites/products/documentation/hpc/mkl/vml/vmldata.htm).
+
+## Supported functions
+
+VML.jl supports the following functions, currently for Float32 and
+Float64 only. While VML also implements operations on complex numbers,
+these are not yet implemented in VML.jl.
+
+### Unary functions
+
+Allocating forms have signature `f(A)`. Mutating forms have signatures
+`f!(A)` (in place) and `f!(out, A)` (out of place).
+
+Allocating | Mutating
+---------------------
+`acos`     | `acos!`
+`asin`     | `asin!`
+`atan`     | `atan!`
+`cos`      | `cos!`
+`sin`      | `sin!`
+`tan`      | `tan!`
+`acosh`    | `acosh!`
+`asinh`    | `asinh!`
+`atanh`    | `atanh!`
+`cosh`     | `cosh!`
+`sinh`     | `sinh!`
+`tanh`     | `tanh!`
+`cbrt`     | `cbrt!`
+`sqrt`     | `sqrt!`
+`exp`      | `expm1!`
+`log`      | `log!`
+`log10`    | `log10!`
+`log1p`    | `log1p!`
+`abs`      | `abs!`
+`abs2`     | `abs2!`
+`ceil`     | `ceil!`
+`floor`    | `floor!`
+`round`    | `round!`
+`trunc`    | `trunc!`
+`erf`      | `erf!`
+`erfc`     | `erfc!`
+`erfinv`   | `erfinv!`
+`efcinv`   | `efcinv!`
+`inv_cbrt` | `inv_cbrt!`
+`inv_sqrt` | `inv_sqrt!`
+`pow2o3`   | `pow2o3!`
+`pow3o2`   | `pow3o2!`
+
+### Binary functions
+
+Allocating forms have signature `f(A, B)`. Mutating forms have
+signature `f!(out, A, B)`. These functions fall back on broadcasting
+when 
+
+Allocating | Mutating
+---------------------
+`atan2`    | `atan2!`
+`hypot`    | `hypot!`
+`.^`       | `pow!`
+`.+`       | `add!`
+`./`       | `divide!`
+`.*`       | `multiply!`
+`.-`       | `subtract!`
+
+## Performance
+
+TODO
--- src/VML.jl
+++ src/VML.jl
@@ -1,5 +1,129 @@
 module VML
 
-# package code goes here
+# TODO detect CPU architecture
+const lib = :libmkl_vml_avx
 
-end # module
+const unary_ops = ((:(Base.acos), :acos!, :Acos),
+                   (:(Base.asin), :asin!, :Asin),
+                   (:(Base.atan), :atan!, :Atan),
+                   (:(Base.cos), :cos!, :Cos),
+                   (:(Base.sin), :sin!, :Sin),
+                   (:(Base.tan), :tan!, :Tan),
+                   (:(Base.acosh), :acosh!, :Acosh),
+                   (:(Base.asinh), :asinh!, :Asinh),
+                   (:(Base.atanh), :atanh!, :Atanh),
+                   (:(Base.cosh), :cosh!, :Cosh),
+                   (:(Base.sinh), :sinh!, :Sinh),
+                   (:(Base.tanh), :tanh!, :Tanh),
+                   (:(Base.cbrt), :cbrt!, :Cbrt),
+                   (:(Base.sqrt), :sqrt!, :Sqrt),
+                   (:(Base.exp), :exp!, :Exp),
+                   (:(Base.expm1), :expm1!, :Expm1),
+                   (:(Base.log), :log!, :Ln),
+                   (:(Base.log10), :log10!, :Log10),
+                   (:(Base.log1p), :log1p, :Log1p),
+                   (:(Base.abs), :abs!, :Abs),
+                   (:(Base.abs2), :abs2!, :Sqr),
+                   (:(Base.ceil), :ceil!, :Ceil),
+                   (:(Base.floor), :floor!, :Floor),
+                   (:(Base.round), :round!, :Round),
+                   (:(Base.trunc), :trunc!, :Trunc),
+                   (:(Base.erf), :erf!, :Erf),
+                   (:(Base.erfc), :erfc!, :Erfc),
+                   (:(Base.erfinv), :erfinv!, :ErfInv),
+                   (:(Base.erfcinv), :erfcinv!, :ErfcInv),
+                   (:(Base.lgamma), :lgamma!, :LGamma),
+                   (:(Base.gamma), :gamma!, :TGamma),
+                   # Not in Base
+                   (:inv_cbrt, :inv_cbrt!, :InvCbrt),
+                   (:inv_sqrt, :inv_sqrt!, :InvSqrt),
+                   (:pow2o3, :pow2o3!, :Pow2o3),
+                   (:pow3o2, :pow3o2!, :Pow3o2))
+
+const binary_vector_ops = ((:(Base.atan2), :atan2!, :Atan2, false),
+                           (:(Base.hypot), :hypot!, :Hypot, false),
+                           (:(Base.(:.^)), :pow!, :Pow, true),
+                           (:(Base.(:.+)), :add!, :Add, true),
+                           (:(Base.(:./)), :divide!, :Div, true),
+                           (:(Base.(:.*)), :multiply!, :Mul, true),
+                           (:(Base.(:.-)), :subtract!, :Sub, true))
+
+for (prefix, t) in ((:_vmls, :Float32), (:_vmld, :Float64))
+    # Unary
+    for (jlname, jlname!, mklname) in unary_ops
+        mklfn = Base.Meta.quot(symbol("$prefix$mklname"))
+        exports = Symbol[]
+        isa(jlname, Expr) || push!(exports, jlname)
+        isa(jlname!, Expr) || push!(exports, jlname!)
+        @eval begin
+            $(isempty(exports) ? nothing : Expr(:export, exports...))
+            function $(jlname!){N}(out::Array{$t,N}, A::Array{$t,N})
+                size(out) == size(A) || throw(DimensionMismatch())
+                ccall(($mklfn, lib), Void, (Int, Ptr{$t}, Ptr{$t}), length(A), A, out)
+                out
+            end
+            function $(jlname!)(A::Array{$t})
+                ccall(($mklfn, lib), Void, (Int, Ptr{$t}, Ptr{$t}), length(A), A, A)
+                A
+            end
+            function $(jlname)(A::Array{$t})
+                out = similar(A)
+                ccall(($mklfn, lib), Void, (Int, Ptr{$t}, Ptr{$t}), length(A), A, out)
+                out
+            end
+        end
+    end
+
+    # Binary, two vectors
+    function (jlname, jlname!, mklname, broadcast) in binary_vector_ops
+        mklfn = Base.Meta.quot(symbol("$prefix$mklname"))
+        isa(jlname, Expr) || push!(exports, jlname)
+        isa(jlname!, Expr) || push!(exports, jlname!)
+        @eval begin
+            $(isempty(exports) ? nothing : Expr(:export, exports...))
+            function $(jlname!){N}(out::Array{$t,N}, A::Array{$t,N}, B::Array{$t,N})
+                size(out) == size(A) == size(B) || $(broadcast ? :(return broadcast!($jlname, out, A, B)) : :(throw(DimensionMismatch())))
+                ccall(($mklfn, lib), Void, (Int, Ptr{$t}, Ptr{$t}, Ptr{$t}), length(A), A, B, out)
+                out
+            end
+            function $(jlname){N}(A::Array{$t,N}, B::Array{$t,N})
+                size(A) == size(B) || $(broadcast ? :(return broadcast($jlname, A, B)) : :(throw(DimensionMismatch())))
+                out = similar(A)
+                ccall(($mklfn, lib), Void, (Int, Ptr{$t}, Ptr{$t}, Ptr{$t}), length(A), A, B, out)
+                out
+            end
+        end
+    end
+
+    # Binary, vector and scalar
+    mklfn = Base.Meta.quot(symbol("$(prefix)pow"))
+    @eval begin
+        export pow!
+        function pow!{N}(out::Array{$t,N}, A::Array{$t,N}, b::$t)
+            size(out) == size(A) || throw(DimensionMismatch())
+            ccall(($mklfn, lib), Void, (Int, Ptr{$t}, $t, Ptr{$t}), length(A), A, b, out)
+            out
+        end
+        function Base.(:(.^)){N}(A::Array{$t,N}, b::$t)
+            out = similar(A)
+            ccall(($mklfn, lib), Void, (Int, Ptr{$t}, $t, Ptr{$t}), length(A), A, b, out)
+            out
+        end
+    end
+end
+
+immutable VMLAccuracy
+    mode::Uint
+end
+const VML_LA = VMLAccuracy(0x00000001)
+const VML_HA = VMLAccuracy(0x00000002)
+const VML_EP = VMLAccuracy(0x00000003)
+Base.show(io::IO, m::VMLAccuracy) = print(io, m == VML_LA ? "VML_LA" :
+                                              m == VML_HA ? "VML_HA" : "VML_EP")
+
+vml_cur_mode() = ccall((:_vmlGetMode, lib), Cuint, ())
+vml_set_accuracy(m::VMLAccuracy) = ccall((:_vmlSetMode, lib), Cuint, (Ptr{Uint},), &(vml_cur_mode() & mode.mode))
+vml_get_accuracy() = VMLAccuracy(vml_cur_mode() & 0x3)
+
+export VML_LA, VML_HA, VML_EP, vml_set_accuracy, vml_get_accuracy
+end
