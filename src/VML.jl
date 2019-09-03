07c9f929e084b4fb7d2a6d636a7c631945c1d907
Steffen <steffen@robots.ox.ac.uk>
2019-09-03 18:14:32 +0200

    Refactor and automatic library detection

--- src/VML.jl
+++ src/VML.jl
@@ -1,121 +1,15 @@
+__precompile__()
+
 module VML
 
 # import Base: .^, ./
+using SpecialFunctions
 using Libdl
-
 # TODO detect CPU architecture
-const lib = :libmkl_vml_avx2
-# Libdl.dlopen(:libmkl_rt)
+# include("libdetect.jl")
+include(joinpath(dirname(@__DIR__), "deps/deps.jl"))
 
-function __init__()
-    Libdl.dlopen(:libmkl_rt, RTLD_LAZY|RTLD_DEEPBIND|RTLD_GLOBAL )
-    Libdl.dlopen(:libmkl_core, RTLD_LAZY|RTLD_DEEPBIND|RTLD_GLOBAL ) #only needed on mac?
-    Libdl.dlopen(lib)
-end
-
-__init__()
-
-struct VMLAccuracy
-    mode::UInt
-end
-const VML_LA = VMLAccuracy(0x00000001)
-const VML_HA = VMLAccuracy(0x00000002)
-const VML_EP = VMLAccuracy(0x00000003)
-Base.show(io::IO, m::VMLAccuracy) = print(io, m == VML_LA ? "VML_LA" :
-                                              m == VML_HA ? "VML_HA" : "VML_EP")
-vml_get_mode() = ccall((:_vmlGetMode, lib), Cuint, ())
-vml_set_mode(mode::Integer) = (ccall((:_vmlSetMode, lib), Cuint, (UInt,), mode); nothing)
-
-vml_set_accuracy(m::VMLAccuracy) = vml_set_mode((vml_get_mode() & ~0x03) | m.mode)
-vml_get_accuracy() = VMLAccuracy(vml_get_mode() & 0x3)
-
-vml_set_mode((vml_get_mode() & ~0x0000FF00))
-function vml_check_error()
-    vml_error = ccall((:_vmlClearErrStatus, lib), Cint, ())
-    if vml_error != 0
-        if vml_error == 1
-            throw(DomainError())
-        elseif vml_error == 2 || vml_error == 3 || vml_error == 4
-            # Singularity, overflow, or underflow
-            # I don't think Base throws on these
-        elseif vml_error == 1000
-            warn("VML does not support $(vml_get_accuracy); lower accuracy used instead")
-        else
-            error("an unexpected error occurred in VML ($vml_error)")
-        end
-    end
-end
-
-function vml_prefix(t::DataType)
-    if t == Float32
-        return "_vmls"
-    elseif t == Float64
-        return "_vmld"
-    elseif t == Complex{Float32}
-        return "_vmlc"
-    elseif t == Complex{Float64}
-        return "_vmlz"
-    end
-    error("unknown type $t")
-end
-
-function def_unary_op(tin, tout, jlname, jlname!, mklname)
-    mklfn = Base.Meta.quot(Symbol("$(vml_prefix(tin))$mklname"))
-    exports = Symbol[]
-#    isa(jlname, Expr) || push!(exports, jlname)
-    (@isdefined jlname) || push!(exports, jlname)
-#    isa(jlname!, Expr) || push!(exports, jlname!)
-    (@isdefined jlname!) || push!(exports, jlname!)
-    @eval begin
-        function ($jlname!)(out::Array{$tout,N}, A::Array{$tin,N}) where {N}
-            size(out) == size(A) || throw(DimensionMismatch())
-            ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tout}), length(A), A, out)
-            vml_check_error()
-            return out
-        end
-        $(if tin == tout
-            quote
-                function $(jlname!)(A::Array{$tin})
-                    ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tout}), length(A), A, A)
-                    vml_check_error()
-                    return A
-                end
-            end
-        end)
-        function ($jlname)(A::Array{$tin})
-            out = similar(A, $tout)
-            ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tout}), length(A), A, out)
-            vml_check_error()
-            return out
-        end
-        $(isempty(exports) ? nothing : Expr(:export, exports...))
-    end
-end
-
-function def_binary_op(tin, tout, jlname, jlname!, mklname, broadcast)
-    mklfn = Base.Meta.quot(Symbol("$(vml_prefix(tin))$mklname"))
-    exports = Symbol[]
-#    isa(jlname, Expr) || push!(exports, jlname)
-    (@isdefined jlname) || push!(exports, jlname)
-#    isa(jlname!, Expr) || push!(exports, jlname!)
-    (@isdefined jlname!) || push!(exports, jlname!)
-    @eval begin
-        $(isempty(exports) ? nothing : Expr(:export, exports...))
-        function ($jlname!)(out::Array{$tout,N}, A::Array{$tin,N}, B::Array{$tin,N}) where {N}
-            size(out) == size(A) == size(B) || $(broadcast ? :(return broadcast!($jlname, out, A, B)) : :(throw(DimensionMismatch())))
-            ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tin}, Ptr{$tout}), length(A), A, B, out)
-            vml_check_error()
-            out
-        end
-        function ($jlname)(A::Array{$tout,N}, B::Array{$tin,N}) where {N}
-            size(A) == size(B) || $(broadcast ? :(return broadcast($jlname, A, B)) : :(throw(DimensionMismatch())))
-            out = similar(A)
-            ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tin}, Ptr{$tout}), length(A), A, B, out)
-            vml_check_error()
-            out
-        end
-    end
-end
+include("setup.jl")
 
 for t in (Float32, Float64, ComplexF32, ComplexF64)
     # Unary, real or complex
@@ -128,8 +22,8 @@ for t in (Float32, Float64, ComplexF32, ComplexF64)
     def_unary_op(t, t, :(Base.log), :log!, :Ln)
 
     # # Binary, real or complex
-    # def_binary_op(t, t, :(.^), :pow!, :Pow, true)
-    # def_binary_op(t, t, :(./), :divide!, :Div, true)
+    def_binary_op(t, t, :pow, :pow!, :Pow, true)
+    def_binary_op(t, t, :divide, :divide!, :Div, true)
 end
 
 for t in (Float32, Float64)
@@ -145,12 +39,12 @@ for t in (Float32, Float64)
     def_unary_op(t, t, :(Base.trunc), :trunc!, :Trunc)
 
     # now in SpecialFunctions (make smart, maybe?)
-    # def_unary_op(t, t, :(Base.erf), :erf!, :Erf)
-    # def_unary_op(t, t, :(Base.erfc), :erfc!, :Erfc)
-    # def_unary_op(t, t, :(Base.erfinv), :erfinv!, :ErfInv)
-    # def_unary_op(t, t, :(Base.erfcinv), :erfcinv!, :ErfcInv)
-    # def_unary_op(t, t, :(Base.lgamma), :lgamma!, :LGamma)
-    # def_unary_op(t, t, :(Base.gamma), :gamma!, :TGamma)
+    def_unary_op(t, t, :(SpecialFunctions.erf), :erf!, :Erf)
+    def_unary_op(t, t, :(SpecialFunctions.erfc), :erfc!, :Erfc)
+    def_unary_op(t, t, :(SpecialFunctions.erfinv), :erfinv!, :ErfInv)
+    def_unary_op(t, t, :(SpecialFunctions.erfcinv), :erfcinv!, :ErfcInv)
+    def_unary_op(t, t, :(SpecialFunctions.lgamma), :lgamma!, :LGamma)
+    def_unary_op(t, t, :(SpecialFunctions.gamma), :gamma!, :TGamma)
     # Not in Base
     def_unary_op(t, t, :inv_cbrt, :inv_cbrt!, :InvCbrt)
     def_unary_op(t, t, :inv_sqrt, :inv_sqrt!, :InvSqrt)
@@ -188,12 +82,12 @@ for t in (Float32, Float64)
     # end
 
     # # Binary, real-only
-    # def_binary_op(t, t, :(Base.atan2), :atan2!, :Atan2, false)
-    # def_binary_op(t, t, :(Base.hypot), :hypot!, :Hypot, false)
+    def_binary_op(t, t, :(Base.atan), :atan!, :Atan, false)
+    def_binary_op(t, t, :(Base.hypot), :hypot!, :Hypot, false)
 
     # Unary, complex-only
     def_unary_op(t, Complex{t}, :(Base.cis), :cis!, :CIS)
-    # def_unary_op(Complex{t}, Complex{t}, :(Base.conj), :conj!, :Conj)
+    def_unary_op(Complex{t}, Complex{t}, :(Base.conj), :conj!, :Conj)
     def_unary_op(Complex{t}, t, :(Base.abs), :abs!, :Abs)
     def_unary_op(Complex{t}, t, :(Base.angle), :angle!, :Arg)
 
---
+++ src/setup.jl
@@ -0,0 +1,109 @@
+
+function __init__()
+    Libdl.dlopen(rtlib, RTLD_GLOBAL)
+    Libdl.dlopen(corelib, RTLD_GLOBAL) # maybe only needed on mac
+    Libdl.dlopen(lib, RTLD_GLOBAL)
+end
+
+__init__()
+
+struct VMLAccuracy
+    mode::UInt
+end
+
+const VML_LA = VMLAccuracy(0x00000001)
+const VML_HA = VMLAccuracy(0x00000002)
+const VML_EP = VMLAccuracy(0x00000003)
+
+
+Base.show(io::IO, m::VMLAccuracy) = print(io, m == VML_LA ? "VML_LA" :
+                                              m == VML_HA ? "VML_HA" : "VML_EP")
+vml_get_mode() = ccall((:_vmlGetMode, lib), Cuint, ())
+vml_set_mode(mode::Integer) = (ccall((:_vmlSetMode, lib), Cuint, (UInt,), mode); nothing)
+
+vml_set_accuracy(m::VMLAccuracy) = vml_set_mode((vml_get_mode() & ~0x03) | m.mode)
+vml_get_accuracy() = VMLAccuracy(vml_get_mode() & 0x3)
+
+vml_set_mode((vml_get_mode() & ~0x0000FF00))
+function vml_check_error()
+    vml_error = ccall((:_vmlClearErrStatus, lib), Cint, ())
+    if vml_error != 0
+        if vml_error == 1
+            throw(DomainError())
+        elseif vml_error == 2 || vml_error == 3 || vml_error == 4
+            # Singularity, overflow, or underflow
+            # I don't think Base throws on these
+        elseif vml_error == 1000
+            warn("VML does not support $(vml_get_accuracy); lower accuracy used instead")
+        else
+            error("an unexpected error occurred in VML ($vml_error)")
+        end
+    end
+end
+
+function vml_prefix(t::DataType)
+    if t == Float32
+        return "_vmls"
+    elseif t == Float64
+        return "_vmld"
+    elseif t == Complex{Float32}
+        return "_vmlc"
+    elseif t == Complex{Float64}
+        return "_vmlz"
+    end
+    error("unknown type $t")
+end
+
+function def_unary_op(tin, tout, jlname, jlname!, mklname)
+    mklfn = Base.Meta.quot(Symbol("$(vml_prefix(tin))$mklname"))
+    exports = Symbol[]
+    (@isdefined jlname) || push!(exports, jlname)
+    (@isdefined jlname!) || push!(exports, jlname!)
+    @eval begin
+        function ($jlname!)(out::Array{$tout,N}, A::Array{$tin,N}) where {N}
+            size(out) == size(A) || throw(DimensionMismatch())
+            ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tout}), length(A), A, out)
+            vml_check_error()
+            return out
+        end
+        $(if tin == tout
+            quote
+                function $(jlname!)(A::Array{$tin})
+                    ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tout}), length(A), A, A)
+                    vml_check_error()
+                    return A
+                end
+            end
+        end)
+        function ($jlname)(A::Array{$tin})
+            out = similar(A, $tout)
+            ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tout}), length(A), A, out)
+            vml_check_error()
+            return out
+        end
+        $(isempty(exports) ? nothing : Expr(:export, exports...))
+    end
+end
+
+function def_binary_op(tin, tout, jlname, jlname!, mklname, broadcast)
+    mklfn = Base.Meta.quot(Symbol("$(vml_prefix(tin))$mklname"))
+    exports = Symbol[]
+    (@isdefined jlname) || push!(exports, jlname)
+    (@isdefined jlname!) || push!(exports, jlname!)
+    @eval begin
+        $(isempty(exports) ? nothing : Expr(:export, exports...))
+        function ($jlname!)(out::Array{$tout,N}, A::Array{$tin,N}, B::Array{$tin,N}) where {N}
+            size(out) == size(A) == size(B) || $(broadcast ? :(return broadcast!($jlname, out, A, B)) : :(throw(DimensionMismatch())))
+            ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tin}, Ptr{$tout}), length(A), A, B, out)
+            vml_check_error()
+            return out
+        end
+        function ($jlname)(A::Array{$tout,N}, B::Array{$tin,N}) where {N}
+            size(A) == size(B) || $(broadcast ? :(return broadcast($jlname, A, B)) : :(throw(DimensionMismatch())))
+            out = similar(A)
+            ccall(($mklfn, lib), Nothing, (Int, Ptr{$tin}, Ptr{$tin}, Ptr{$tout}), length(A), A, B, out)
+            vml_check_error()
+            return out
+        end
+    end
+end
