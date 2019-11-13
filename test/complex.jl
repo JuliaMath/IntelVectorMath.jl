647862a6751edf74cf602fbd5be859d7b714dccd
Michael Abbott <me@escbook>
2019-11-13 20:16:38 +0100

    tests pass

--- test/complex.jl
+++ test/complex.jl
@@ -1,18 +1,30 @@
 # First generate some random data and test functions in Base on it
 const NVALS = 1000
-input = Dict(t=>[(randindomain(t, NVALS, domain),) for (_, domain) in base_unary_complex]
-            for t in (ComplexF32, ComplexF64))
+
+input = Dict(
+    t=>[ (randindomain(t, NVALS, domain),) for (_, domain) in base_unary_complex ]
+        for t in (ComplexF32, ComplexF64)
+)
+
 fns = [x[1] for x in base_unary_complex]
-output = Dict(t=>[fns[i](input[t][i]...) for i = 1:length(fns)] 
-    for t in (ComplexF32, ComplexF64))
+
+# output = Dict(
+#     t=>[ fns[i](input[t][i]...) for i = 1:length(fns) ]
+#         for t in (ComplexF32, ComplexF64)
+# )
 
 @testset "Definitions and Comparison with Base for Complex" begin
 
   for t in (ComplexF32, ComplexF64), i = 1:length(fns)
-    fn = fns[i]
-    Test.@test which(fn, typeof(input[t][i])).module == VML
+
+    base_fn = fns[i]
+    vml_fn = eval(:(VML.$(Symbol(base_fn))))
+
+    Test.@test which(vml_fn, typeof(input[t][i])).module == VML
+
     # Test.test_approx_eq(output[t][i], fn(input[t][i]...), "Base $t $fn", "VML $t $fn")
-    Test.@test fn(input[t][i]...) ≈ fn.(input[t][i]...)
+    Test.@test vml_fn(input[t][i]...) ≈ base_fn.(input[t][i]...)
+
   end
 
 end
--- test/real.jl
+++ test/real.jl
@@ -1,21 +1,31 @@
 # First generate some random data and test functions in Base on it
 const NVALS = 1000
-input = Dict(t=>[[(randindomain(t, NVALS, domain),) for (_, domain) in base_unary_real];
-             [(randindomain(t, NVALS, domain1), randindomain(t, NVALS, domain2))
-              for (_, domain1, domain2) in base_binary_real]]
-            for t in (Float32, Float64))
-fns = [[x[1] for x in base_unary_real]; [x[1] for x in base_binary_real]]
-# output = Dict(t=>[fns[i](input[t][i]...) for i = 1:length(fns)] for t in (Float32, Float64))
 
+input = Dict(
+    t=>[
+        [ (randindomain(t, NVALS, domain),) for (_, domain) in base_unary_real ];
+        [ (randindomain(t, NVALS, domain1), randindomain(t, NVALS, domain2))
+            for (_, domain1, domain2) in base_binary_real ]
+    ]
+    for t in (Float32, Float64)
+)
+
+fns = [[x[1] for x in base_unary_real]; [x[1] for x in base_binary_real]]
+
+# output = Dict(t=>[fns[i](input[t][i]...) for i = 1:length(fns)] for t in (Float32, Float64))
 
 @testset "Definitions and Comparison with Base for Reals" begin
 
   for t in (Float32, Float64), i = 1:length(fns)
-    
-    fn = fns[i]
-    Test.@test which(fn, typeof(input[t][i])).module == VML
+
+    base_fn = fns[i]
+    vml_fn = eval(:(VML.$(Symbol(base_fn))))
+
+    Test.@test which(vml_fn, typeof(input[t][i])).module == VML
+
     # Test.test_approx_eq(output[t][i], fn(input[t][i]...), "Base $t $fn", "VML $t $fn")
-    Test.@test fn(input[t][i]...) ≈ fn.(input[t][i]...)
+    Test.@test vml_fn(input[t][i]...) ≈ base_fn.(input[t][i]...)
+
   end
 
 end
@@ -23,11 +33,13 @@ end
 @testset "Error Handling and Settings" begin
 
   # Verify that we still throw DomainErrors
-  Test.@test_throws DomainError sqrt([-1.0])
+  Test.@test_throws DomainError VML.sqrt([-1.0])
+  Test.@test_throws DomainError VML.log([-1.0])
 
   # Setting accuracy
   vml_set_accuracy(VML_LA)
   Test.@test vml_get_accuracy() == VML_LA
+
   vml_set_accuracy(VML_EP)
   Test.@test vml_get_accuracy() == VML_EP
 
