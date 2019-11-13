a41c695b87dc8987457f4e3d786b37bd95e5aee1
Michael Abbott <me@escbook>
2019-11-13 14:35:20 +0100

    first pass, remove Base., simple overload macro

--- src/VML.jl
+++ src/VML.jl
@@ -13,13 +13,13 @@ include("setup.jl")
 
 for t in (Float32, Float64, ComplexF32, ComplexF64)
     # Unary, real or complex
-    def_unary_op(t, t, :(Base.acos), :acos!, :Acos)
-    def_unary_op(t, t, :(Base.asin), :asin!, :Asin)
-    def_unary_op(t, t, :(Base.acosh), :acosh!, :Acosh)
-    def_unary_op(t, t, :(Base.asinh), :asinh!, :Asinh)
-    def_unary_op(t, t, :(Base.sqrt), :sqrt!, :Sqrt)
-    def_unary_op(t, t, :(Base.exp), :exp!, :Exp)
-    def_unary_op(t, t, :(Base.log), :log!, :Ln)
+    def_unary_op(t, t, :acos, :acos!, :Acos)
+    def_unary_op(t, t, :asin, :asin!, :Asin)
+    def_unary_op(t, t, :acosh, :acosh!, :Acosh)
+    def_unary_op(t, t, :asinh, :asinh!, :Asinh)
+    def_unary_op(t, t, :sqrt, :sqrt!, :Sqrt)
+    def_unary_op(t, t, :exp, :exp!, :Exp)
+    def_unary_op(t, t, :log, :log!, :Ln)
 
     # # Binary, real or complex
     def_binary_op(t, t, :pow, :pow!, :Pow, true)
@@ -28,23 +28,23 @@ end
 
 for t in (Float32, Float64)
     # Unary, real-only
-    def_unary_op(t, t, :(Base.cbrt), :cbrt!, :Cbrt)
-    def_unary_op(t, t, :(Base.expm1), :expm1!, :Expm1)
-    def_unary_op(t, t, :(Base.log1p), :log1p, :Log1p)
-    def_unary_op(t, t, :(Base.abs), :abs!, :Abs)
-    def_unary_op(t, t, :(Base.abs2), :abs2!, :Sqr)
-    def_unary_op(t, t, :(Base.ceil), :ceil!, :Ceil)
-    def_unary_op(t, t, :(Base.floor), :floor!, :Floor)
-    def_unary_op(t, t, :(Base.round), :round!, :Round)
-    def_unary_op(t, t, :(Base.trunc), :trunc!, :Trunc)
+    def_unary_op(t, t, :cbrt, :cbrt!, :Cbrt)
+    def_unary_op(t, t, :expm1, :expm1!, :Expm1)
+    def_unary_op(t, t, :log1p, :log1p, :Log1p)
+    def_unary_op(t, t, :abs, :abs!, :Abs)
+    def_unary_op(t, t, :abs2, :abs2!, :Sqr)
+    def_unary_op(t, t, :ceil, :ceil!, :Ceil)
+    def_unary_op(t, t, :floor, :floor!, :Floor)
+    def_unary_op(t, t, :round, :round!, :Round)
+    def_unary_op(t, t, :trunc, :trunc!, :Trunc)
 
     # now in SpecialFunctions (make smart, maybe?)
-    def_unary_op(t, t, :(SpecialFunctions.erf), :erf!, :Erf)
-    def_unary_op(t, t, :(SpecialFunctions.erfc), :erfc!, :Erfc)
-    def_unary_op(t, t, :(SpecialFunctions.erfinv), :erfinv!, :ErfInv)
-    def_unary_op(t, t, :(SpecialFunctions.erfcinv), :erfcinv!, :ErfcInv)
-    def_unary_op(t, t, :(SpecialFunctions.lgamma), :lgamma!, :LGamma)
-    def_unary_op(t, t, :(SpecialFunctions.gamma), :gamma!, :TGamma)
+    def_unary_op(t, t, :erf, :erf!, :Erf)
+    def_unary_op(t, t, :erfc, :erfc!, :Erfc)
+    def_unary_op(t, t, :erfinv, :erfinv!, :ErfInv)
+    def_unary_op(t, t, :erfcinv, :erfcinv!, :ErfcInv)
+    def_unary_op(t, t, :lgamma, :lgamma!, :LGamma)
+    def_unary_op(t, t, :gamma, :gamma!, :TGamma)
     # Not in Base
     def_unary_op(t, t, :inv_cbrt, :inv_cbrt!, :InvCbrt)
     def_unary_op(t, t, :inv_sqrt, :inv_sqrt!, :InvSqrt)
@@ -53,15 +53,15 @@ for t in (Float32, Float64)
 
     # Enabled only for Real. MKL guarantees higher accuracy, but at a
     # substantial performance cost.
-    def_unary_op(t, t, :(Base.atan), :atan!, :Atan)
-    def_unary_op(t, t, :(Base.cos), :cos!, :Cos)
-    def_unary_op(t, t, :(Base.sin), :sin!, :Sin)
-    def_unary_op(t, t, :(Base.tan), :tan!, :Tan)
-    def_unary_op(t, t, :(Base.atanh), :atanh!, :Atanh)
-    def_unary_op(t, t, :(Base.cosh), :cosh!, :Cosh)
-    def_unary_op(t, t, :(Base.sinh), :sinh!, :Sinh)
-    def_unary_op(t, t, :(Base.tanh), :tanh!, :Tanh)
-    def_unary_op(t, t, :(Base.log10), :log10!, :Log10)
+    def_unary_op(t, t, :atan, :atan!, :Atan)
+    def_unary_op(t, t, :cos, :cos!, :Cos)
+    def_unary_op(t, t, :sin, :sin!, :Sin)
+    def_unary_op(t, t, :tan, :tan!, :Tan)
+    def_unary_op(t, t, :atanh, :atanh!, :Atanh)
+    def_unary_op(t, t, :cosh, :cosh!, :Cosh)
+    def_unary_op(t, t, :sinh, :sinh!, :Sinh)
+    def_unary_op(t, t, :tanh, :tanh!, :Tanh)
+    def_unary_op(t, t, :log10, :log10!, :Log10)
 
     # # .^ to scalar power
     # mklfn = Base.Meta.quot(Symbol("$(vml_prefix(t))Powx"))
@@ -82,22 +82,36 @@ for t in (Float32, Float64)
     # end
 
     # # Binary, real-only
-    def_binary_op(t, t, :(Base.atan), :atan!, :Atan2, false)
-    def_binary_op(t, t, :(Base.hypot), :hypot!, :Hypot, false)
+    def_binary_op(t, t, :atan, :atan!, :Atan2, false)
+    def_binary_op(t, t, :hypot, :hypot!, :Hypot, false)
 
     # Unary, complex-only
-    def_unary_op(Complex{t}, Complex{t}, :(Base.conj), :conj!, :Conj)
-    def_unary_op(Complex{t}, t, :(Base.abs), :abs!, :Abs)
-    def_unary_op(Complex{t}, t, :(Base.angle), :angle!, :Arg)
+    def_unary_op(Complex{t}, Complex{t}, :conj, :conj!, :Conj)
+    def_unary_op(Complex{t}, t, :abs, :abs!, :Abs)
+    def_unary_op(Complex{t}, t, :angle, :angle!, :Arg)
 
     # Binary, complex-only. These are more accurate but performance is
     # either equivalent to Base or slower.
-    # def_binary_op(Complex{t}, Complex{t}, :(Base.(:+)), :add!, :Add, false)
-    # def_binary_op(Complex{t}, Complex{t}, :(Base.(:.+)), :add!, :Add, true)
-    # def_binary_op(Complex{t}, Complex{t}, :(Base.(:.*)), :multiply!, :Mul, true)
-    # def_binary_op(Complex{t}, Complex{t}, :(Base.(:-)), :subtract!, :Sub, false)
-    # def_binary_op(Complex{t}, Complex{t}, :(Base.(:.-)), :subtract!, :Sub, true)
+    # def_binary_op(Complex{t}, Complex{t}, (:+), :add!, :Add, false)
+    # def_binary_op(Complex{t}, Complex{t}, (:.+), :add!, :Add, true)
+    # def_binary_op(Complex{t}, Complex{t}, (:.*), :multiply!, :Mul, true)
+    # def_binary_op(Complex{t}, Complex{t}, (:-), :subtract!, :Sub, false)
+    # def_binary_op(Complex{t}, Complex{t}, (:.-), :subtract!, :Sub, true)
     # def_binary_op(Complex{t}, Complex{t}, :multiply_conj, :multiply_conj!, :Mul, false)
+end
+
+"""
+    @overload exp log sin
+
+This macro adds a method to each function, so that when acting on an array,
+it calls the `VML` version. The existing action on scalars is unaffected.
+
+That is, it defines  `Base.exp(A::AbstractArray) = VML.exp(A)` etc,
+while leaving `exp.(A)` alone.
+"""
+macro overload(funs...)
+    defs = [:( Base.$f(A::AbstractArray) = VML.$f(A) ) for f in funs ]
+    esc(Expr(:block, defs...))
 end
 
 export VML_LA, VML_HA, VML_EP, vml_set_accuracy, vml_get_accuracy
