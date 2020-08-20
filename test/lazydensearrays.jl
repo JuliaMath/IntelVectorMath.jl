3a04641483090bc41fb6afc662c9c15784a087cb
N3N5 <69878651+N3N5@users.noreply.github.com>
2020-08-21 02:30:50 +0800

    Create lazydensearrays.jl

---
+++ test/lazydensearrays.jl
@@ -0,0 +1,18 @@
+using LinearAlgebra
+
+@testset "lazy_dense_array" begin
+    A = randn(100)
+    B = IVM.sin(A)
+    A′ = @view A[:,1]
+    B′ = IVM.sin(A′)
+    @test B == B′
+    A′ = @views reshape(A[:],20,5)
+    B′ = reshape(IVM.sin(A′),size(B))
+    @test B == B′
+    A′ = @views reshape(A[:],20,5)
+    B′ = IVM.sin(A')'
+    @test B == B′[:,1]
+
+    @test_throws ArgumentError IVM.sin(view(A,1:2:100))
+    @test_throws ArgumentError IVM.abs(rand(ComplexF32,100)')
+end
