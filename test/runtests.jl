bf37b33437caffd8da0f550f88380c205a1f1ac1
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-02-23 18:01:16 -0500

    Use older MKL on Intel Macs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,17 @@
+import Pkg
+using Pkg
+
+if Sys.isapple() && Sys.ARCH == :x86_64
+    Pkg.add(name="MKL_jll", version="2023");
+    Pkg.pin(name="MKL_jll", version="2023");
+end
+
 import MKL_jll
+if !MKL_jll.is_available()
+    @warn "MKL is not available/installed. Exiting."
+    exit()
+end
+
 using Test
 using IntelVectorMath
 using SpecialFunctions
